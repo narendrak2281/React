beba29bcbc27b350d0274be867fd954dbe8be5af
Narendra-Kachare-02 <narendra.kachare@techcreants.com>
2024-12-22 16:20:47 +0530

    useEffect hooks with return function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState} from 'react';
+
+import MyComponent from './MyComp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [isVisible, setVisible] = useState(true)
+
+  useEffect(() => {
+    console.log("App component is mounting...")
+  }, [])
+
+  return <div className="App">
+    {isVisible ? <MyComponent/> : <></>} 
+    <button onClick={() => setVisible(!isVisible)} >Toggle</button>
+  </div>
+      
 }
 
 export default App;
---
+++ src/MyComp.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+const MyComponent = () => {
+
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(10);
+
+    useEffect(() => {
+        console.log("My Component is mounting....")
+
+        return function () {
+            console.log("Unmounting.....")
+        };
+    }, [])
+
+    useEffect(() => {
+        console.log("Count got updated",count)
+    }, [count, count2])
+
+
+    return (
+        <div>
+            <p>Count is {count}</p>
+            <p>Count2 is {count2}</p>
+            <button onClick={() => setCount(count + 1)} >Update</button>
+            <button onClick={() => setCount2(count2 + 1)} >Update 2</button>
+        </div>
+    )
+}
+
+export default MyComponent;
