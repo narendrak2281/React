ed9628c5ad16dcb43877b15fba4f2cceecd54e32
Narendra-Kachare-02 <narendra.kachare@techcreants.com>
2024-12-22 13:15:17 +0530

    One component with multiple state

--- src/components/CounterComponent.jsx
+++ src/components/CounterComponent.jsx
@@ -2,12 +2,16 @@ import React, {useState} from "react";
 
 const CounterComponent = () => {
     const [count, setCount] = useState(15);
+    const [value, setValue] = useState("Narendra");
 
     console.log("Hello", count)
+
 
     return (
         <div>
             <p>Count component -  {count}</p>
+            <h6>Value is {value}</h6>
+            <button onClick={() => setValue("React")} >Change</button>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)} >Decrement</button>
         </div>
