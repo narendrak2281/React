d43b42d7b14ddf8767a0f7d76bfb7bc27a1eb0a0
Narendra-Kachare-02 <narendra.kachare@techcreants.com>
2024-12-23 16:48:49 +0530

    fetch random user get api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import { useState, useEffect } from 'react';
-import {getPosts} from './api'
+import {getPosts, getRandomUser} from './api'
 import './App.css';
 
 import PostCard from './components/PostCard';
+import UserCard from './components/UserCard';
 
 function App() {
 
   const [data, setData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     getPosts().then((posts) => setData(posts));
   }, [])
-  
+
+  useEffect(() => {
+    getRandomUser().then((user) => setUserData(user.results[0]));
+  }, []);
+
+  const refresh = () => {
+    getRandomUser().then((user) => setUserData(user.results[0]));
+  }
+
   return (
 
     <div className="App">
+      {userData && <UserCard data={userData} />}
+      <button onClick={refresh} >Refresh User</button>
       {
         data ? data.map((e) => <PostCard title={e.title} body={e.body} />) : <p>No Data</p>
       }
