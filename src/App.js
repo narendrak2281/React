22650815f9763d3ed49e3d33e8426b2e9baab93b
Narendra-Kachare-02 <narendra.kachare@techcreants.com>
2024-12-23 16:10:05 +0530

    get API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import {getPosts} from './api'
 import './App.css';
 
+import PostCard from './components/PostCard';
+
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getPosts().then((posts) => setData(posts));
+  }, [])
+  
   return (
+
     <div className="App">
-      
+      {
+        data ? data.map((e) => <PostCard title={e.title} body={e.body} />) : <p>No Data</p>
+      }
     </div>
+
   );
 }
 
